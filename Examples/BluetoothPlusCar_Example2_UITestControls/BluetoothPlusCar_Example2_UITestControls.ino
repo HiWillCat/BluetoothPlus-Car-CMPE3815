82c810029d2cf4fe8be864383b71913cd5b6bea2
HiWillCat <wilgam.edu@gmail.com>
2024-11-19 16:18:13 -0500

    Add files via upload

---
+++ Examples/BluetoothPlusCar_Example2_UITestControls/BluetoothPlusCar_Example2_UITestControls.ino
@@ -0,0 +1,70 @@
+// === FINAL PROJECT - Bluetooth+ Car === //
+
+// EXAMPLE 2 - RemoteXY GUI Test
+  // 
+
+// ============================================== //
+
+//////////////////////////////////////////////
+//        RemoteXY include library          //
+//////////////////////////////////////////////
+
+// you can enable debug logging to Serial at 115200
+//#define REMOTEXY__DEBUGLOG    
+
+// RemoteXY select connection mode and include library 
+#define REMOTEXY_MODE__HARDSERIAL
+
+
+// RemoteXY connection settings 
+#define REMOTEXY_SERIAL Serial
+#define REMOTEXY_SERIAL_SPEED 9600
+
+
+#include <RemoteXY.h>
+
+// RemoteXY GUI configuration  
+#pragma pack(push, 1)  
+uint8_t RemoteXY_CONF[] =   // 29 bytes
+  { 255,2,0,0,0,22,0,19,0,0,0,0,31,1,106,200,1,1,1,0,
+  5,23,17,60,60,0,2,26,31 };
+  
+// this structure defines all the variables and events of your control interface 
+struct {
+
+    // input variables
+  int8_t joystick_01_x; // from -100 to 100
+  int8_t joystick_01_y; // from -100 to 100
+
+    // other variable
+  uint8_t connect_flag;  // =1 if wire connected, else =0
+
+} RemoteXY;   
+#pragma pack(pop)
+ 
+/////////////////////////////////////////////
+//           END RemoteXY include          //
+/////////////////////////////////////////////
+
+
+
+void setup() 
+{
+  RemoteXY_Init (); 
+  
+  
+  // TODO you setup code
+  
+}
+
+void loop() 
+{ 
+  RemoteXY_Handler ();
+  
+  
+  // TODO you loop code
+  // use the RemoteXY structure for data transfer
+  // do not call delay(), use instead RemoteXY_delay() 
+
+
+}
